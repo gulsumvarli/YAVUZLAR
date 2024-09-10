85365669b58954fb4c0cb7527e59c48f9442eb23
Ümmü Gülsüm Varlı <93592493+gulsumvarli@users.noreply.github.com>
2024-09-10 15:22:07 +0300

    Delete task2-questapp/utils directory

--- task2-questapp/utils/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-try {
-    $db = new PDO('sqlite:C:\xampp\htdocs\web-server\deneme.db');
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (Exception $e) {
-    echo "Bağlantı hatası: " . $e->getMessage();
-    exit();
-}
